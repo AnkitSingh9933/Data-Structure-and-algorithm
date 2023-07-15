871b96fa7f2ed3628f72f429f31cf60861ae1cf9
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2023-07-15 07:01:43 -0700

    Binary Search Basic.cpp
    T.C-O(logn)
    S.C-O(1);
    
    Source VSCODE-- Binary Search Is Better Then Linear Search;

--- Binary Search/Binary Search Basic.cpp
+++ Binary Search/Binary Search Basic.cpp
@@ -1,35 +1,35 @@
-
-
-int findDuplicate(vector<int> &arr) 
-
+#include <iostream>
+using namespace std;
+int binarysearch(int arr[], int n, int key)
 {
+    int s = 0;
+    int e = n - 1;
 
-   int ans = 0;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
 
-   
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10;
+    int key = 5;
 
-   for(int i = 0; i < arr.size(); i++){
-
-     
-
-     ans = ans^arr[i];
-
-     
-
-   }
-
-   for(int i = 1; i < arr.size(); i++){
-
-     
-
-     ans = ans^i;
-
-     
-
-   }
-
-  
-
-  return ans;
-
+    int ans = binarysearch(arr, n, key);
+    cout << "Inedx is : " << ans;
 }

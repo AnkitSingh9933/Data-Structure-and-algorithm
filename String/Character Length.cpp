de32f99c97a969056e07b4766e8dd75d78b42000
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2023-08-02 05:54:43 -0700

    Character Array Length.cpp
    T.C--O(n);
    S.C--O(1);
    
    Source--VS CODE;

---
+++ String/Character Length.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int getlength(char name[]){
+    int count=0;
+    int i=0;
+   while(name[i]!='\0'){
+    count++;
+     i++;
+   }
+   return count;
+  
+}
+
+int main(){
+
+    char name[10];
+    cout<<"Enter The Name : ";
+    cin>>name;
+    
+    cout<<"Name is : "<<name<<endl;
+
+   int ans=getlength(name);
+   cout<<ans;
+   
+}

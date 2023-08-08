f7afe6147ef5ef7cbfcd75f4f98646b35c4f3896
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2023-08-08 07:57:20 -0700

    Repplace Spaces(using extra space).cpp
    T.C--O(n);
    S.C--O(n);
    
    https://www.codingninjas.com/studio/problems/replace-spaces_1172172?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_5&leftPanelTab=0

---
+++ Coding Ninjas Problem Solve/Repplace Spaces(using extra space).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+//using Extra Space;
+string replaceSpaces(string &str){
+	
+	string temp = "";
+
+	for(int i=0; i<str.size(); i++){
+		
+		if(str[i] == ' '){
+			
+			temp.push_back('@');
+			temp.push_back('4');
+			temp.push_back('0');
+			
+		}
+		else{
+			
+			temp.push_back(str[i]);
+			
+		}
+	}
+	return temp;
+}

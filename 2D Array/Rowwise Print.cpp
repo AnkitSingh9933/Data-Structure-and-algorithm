c3aa01522069b139ed6c47be5cef812f9cead023
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2023-11-13 23:00:35 -0800

    Rowwise Print.cpp
    T.C-- O(i * j);
    S.C--O(i * j);

---
+++ 2D Array/Rowwise Print.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Takin input rowwise
+    int i,j;
+    cout<<"Enter The Value Of  Row and Col ";
+    cin>>i>>j;
+    int arr[i][j];
+    cout<<"Enter The Number For Print : ";
+    for(int row=0; row<i; row++){
+        for(int col=0; col<j; col++){
+            cin>>arr[row][col];
+        }
+    }
+
+     for(int row=0; row<i; row++){
+        for(int col=0; col<j; col++){
+            cout<<arr[row][col]<<" ";
+        }cout<<endl;
+    }
+}

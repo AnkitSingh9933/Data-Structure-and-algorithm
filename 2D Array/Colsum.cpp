d846c90c981caf85006b153bb6ac4aba9675c6c6
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2023-11-14 22:53:57 -0800

    Colsum.cpp
    T.C--O(1);
    S.C--O(12);

---
+++ 2D Array/Colsum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void printraw(int arr[][4], int raw, int col)
+{
+    for (int col = 0; col < 4; col++)
+    {
+        int sum = 0;
+        for (int raw = 0; raw < 3; raw++)
+        {
+              
+            sum += arr[col][raw];
+            
+        }
+        cout << sum<<" ";
+       
+    }
+}
+
+// Taking Raw Wise Input
+int main()
+{
+
+    int arr[3][4];
+
+    cout<<"Enter The Num For Rowwisesum : ";
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << " Rowwise Sum Is ";
+    printraw(arr, 3, 4);
+}
